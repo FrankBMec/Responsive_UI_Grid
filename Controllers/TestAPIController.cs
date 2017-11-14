132232bee39f500451f6299a47527ca3d7869671
Bhavin Mec <30741139+FrankBMec@users.noreply.github.com>
2017-11-14 09:50:50 +0530

    Initial commit

---
+++ Controllers/TestAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Responsive_UI_Grid.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Responsive_UI_Grid.Controllers
+{
+    public class TestAPIController : ApiController
+    {
+       [Route("getData")]
+        [HttpGet]
+        public int getDatass(int dm)
+        {
+            return 1;
+        }
+    }
+}
---
+++ Models/DemoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Responsive_UI_Grid.Models
+{
+    public class DemoModel
+    {
+        public string name { get; set; }
+        public string name1 { get; set; }
+    }
+}
